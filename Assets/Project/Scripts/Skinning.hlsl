0e0af7381aeaec8dccc725825aa485dbb00d5a18
jred1 <cascinojared@gmail.com>
2022-07-12 20:34:30 -0400

    add dual quaternion skinning

--- Assets/Project/Scripts/Skinning.hlsl
+++ Assets/Project/Scripts/Skinning.hlsl
@@ -2,8 +2,13 @@
 #ifndef VOXEL_MESH_INFO
 #define VOXEL_MESH_INFO
 
+#include "Assets/Project/Scripts/DualQuaternion.hlsl"
+
 StructuredBuffer<float4x4> bones;
 StructuredBuffer<float4x4> bindPose;
+
+StructuredBuffer<dualQuat> bonesDQ;
+StructuredBuffer<dualQuat> bindPoseDQ;
 
 void Direct_float(float4 pos, float4 norm, float4 bone_ids, float4 bone_weights, out float4 v, out float4 n)
 {
@@ -47,6 +52,31 @@ void Linear_Opt_float(float4 pos, float4 norm, float4 bone_ids, float4 bone_weig
 
     v = mul(relativeTransform, pos);
     n = mul(relativeTransform, norm);
+}
+
+void Dual_Quaternion_float(float3 pos, float3 norm, float4 bone_ids, float4 bone_weights, float bone_count, out float3 v, out float3 n)
+{
+    int i;
+    float totalWeight = 0;
+    for(i = 0; i < bone_count; i++){
+        totalWeight +=  bone_weights[i];
+    }
+
+    dualQuat dq = dq_mul(bonesDQ[bone_ids[0]], bindPoseDQ[bone_ids[0]]);
+    float4 q0 = dq.qReal;
+    
+    dualQuat dq_blend = dq_mul_s((bone_weights[0] / totalWeight), dq);
+    for(i = 1; i < bone_count; i++){
+        dq = dq_mul(bonesDQ[bone_ids[i]], bindPoseDQ[bone_ids[i]]);
+        float w = bone_weights[i];
+        
+        if(dot(dq.qReal,q0) < 0.0f )
+                w *= -1.0f;
+
+        dq_blend = dq_add(dq_blend, dq_mul_s((w / totalWeight), dq));
+    }
+    v = move(dq_blend,pos);
+    n = dq_rotate(dq_blend,norm);
 }
 
 #endif
